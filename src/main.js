fc1b1627940dd20657d15b35de6d4c3babdb1dca
NachoKai <nachokai@outlook.com>
2020-04-20 01:13:36 -0300

    .

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
 let reactBar = document.getElementById("react-bar");
+let vueBar = document.getElementById("vue-bar");
+let nodeBar = document.getElementById("node-bar");
+let mongoBar = document.getElementById("mongo-bar");
+let apiBar = document.getElementById("api-bar");
+let cypressBar = document.getElementById("cypress-bar");
+
 RPGUI.set_value(reactBar, 0.5);
+RPGUI.set_value(vueBar, 0.5);
+RPGUI.set_value(nodeBar, 0.5);
+RPGUI.set_value(mongoBar, 0.5);
+RPGUI.set_value(apiBar, 0.5);
+RPGUI.set_value(cypressBar, 0.5);
