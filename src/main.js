f4cdea455b2cfecccf909ddbc8c14e9c61526c7f
NachoKai <nachokai@outlook.com>
2020-05-05 15:27:58 -0300

    add more info and year

---
+++ src/main.js
@@ -0,0 +1,6 @@
+const date = new Date();
+let year = date.getFullYear();
+let myAge = year - 1992;
+
+document.getElementById("age").innerText = myAge;
+document.getElementById("year").innerText = year;
