118835bdb0f17b62e0574271115a1cd3ce47f07f
Nacho Caiafa <nachokai@outlook.com>
2020-05-25 14:45:32 -0300

    add age function

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
-const date = new Date();
-let year = date.getFullYear();
-let myAge = year - 1992;
+const today = new Date().toISOString().split('T')[0];
+const myYear = '1992';
+const myMonth = '02';
+const myDay = '08';
+const currentYear = today.split('').splice(0, 4).join('');
+const currentMonth = today.split('').splice(5, 2).join('');
+const currentDay = today.split('').splice(8, 2).join('');
 
-document.getElementById("age").innerText = myAge;
-document.getElementById("year").innerText = year;
+const myAge = (currentYear, myYear) => {
+  if (currentMonth <= myMonth && currentDay <= myDay) {
+    return currentYear - myYear - 1;
+  } else {
+    return currentYear - myYear;
+  }
+};
+
+document.getElementById('age').innerText = myAge(currentYear, myYear);
+document.getElementById('year').innerText = currentYear;
